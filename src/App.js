ab413d97d5dab753d5a6a04decab94693c7a0c62
Danielestra <166335769+Danielestra@users.noreply.github.com>
2024-11-18 21:29:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <div className="App">
       <h1>Pomodoro Timer</h1>
+      <Timer />
     </div>
   );
 }
